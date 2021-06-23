f789e228c54442f6b007c9ced0dc4484b2f752da
George Thomas <george@redmoor.io>
2021-06-23 08:45:27 +0100

    shop demo

---
+++ pages/api/checkout_session.ts
@@ -0,0 +1,45 @@
+import Stripe from 'stripe';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const stripe = new Stripe('sk_test_51J1TyKLqUjiXusfCjAaSX7pAc37us8BatbmfbnGeeF7M7aCjETYAJz6YYY5b9Syv3faZqlbNjI7435C28O7YuwPO00zmUOqDUM');
+
+const YOUR_DOMAIN = 'http://localhost:3000/drill';
+
+
+type Data = {
+    id: string
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+            {
+                price_data: {
+                    currency: 'gbp',
+                    product_data: {
+                        name: 'Vegetarian Drill Supper',
+                        images: ['https://images.immediate.co.uk/production/volatile/sites/30/2020/08/caponata-pasta_1-197841f.jpg?webp=true&quality=90&resize=500%2C454'],
+                    },
+                    unit_amount: 500,
+                },
+                quantity: 2,
+            },
+            {
+                price_data: {
+                    currency: 'gbp',
+                    product_data: {
+                        name: 'Meat Drill Supper',
+                        images: ['https://images.immediate.co.uk/production/volatile/sites/30/2020/08/coconut-squash-dhansak-a3a9133.jpg?quality=90&webp=true&resize=600,545'],
+                    },
+                    unit_amount: 500,
+                },
+                quantity: 1,
+            },
+        ],
+        mode: 'payment',
+        success_url: `${YOUR_DOMAIN}?success=true`,
+        cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    });
+    res.json({ id: session.id });
+}
---
+++ pages/drill.tsx
@@ -0,0 +1,67 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { loadStripe } from "@stripe/stripe-js";
+import styles from '../styles/Home.module.css'
+
+const stripePromise = loadStripe("pk_test_51J1TyKLqUjiXusfCouAMNY7rpc8oqlT2Nnmlg2fKdNX1MKxh6KRjGY264kYrFcgKE9KF9VNVSNi9CmVVa7rRrJZ200XFNM9APy");
+
+export default function Drill() {
+
+    const handleClick = async (event) => {
+        const stripe = await stripePromise;
+        const response = await fetch("/api/checkout_session", {
+            method: "POST",
+        });
+        const session = await response.json();
+        // When the customer clicks on the button, redirect them to Checkout.
+        const result = await stripe.redirectToCheckout({
+            sessionId: session.id,
+        });
+        if (result.error) {
+            // If `redirectToCheckout` fails due to a browser or network
+            // error, display the localized error message to your customer
+            // using `result.error.message`.
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Drill Suppers</title>
+                <meta name="description" content="Buy a drill supper" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className={styles.main}>
+                <form method="POST">
+                    <h1>Booking</h1>
+                    <fieldset style={{ padding: '1em', marginBottom: '2em' }}>
+                        <legend>Person</legend>
+                        <div>
+                            <label htmlFor="name" style={{ display: 'block', width: '250px' }}>Name</label>
+                            <input id="name" type="text" style={{ display: 'block', width: '250px' }} />
+                        </div>
+                        <div style={{ marginTop: '2em' }}>
+                            <label htmlFor="army-number" style={{ display: 'block', width: '250px' }}>Army Number</label>
+                            <input id="army-number" type="text" style={{ display: 'block', width: '250px' }} />
+                        </div>
+                    </fieldset>
+                    <fieldset style={{ padding: '1em' }}>
+                        <legend>Meals</legend>
+                        <div>
+                            <label htmlFor="meat-meal" style={{ display: 'block', width: '250px' }}>Non-Vegetarian</label>
+                            <input id="meat-meal" type="integer" style={{ display: 'block', width: '250px' }} />
+                        </div>
+                        <div style={{ marginTop: '2em' }}>
+                            <label htmlFor="veg-meal" style={{ display: 'block', width: '250px' }}>Vegeratian</label>
+                            <input id="veg-meal" type="integer" style={{ display: 'block', width: '250px' }} />
+                        </div>
+                    </fieldset>
+                    <button type="button" id="checkout-button" role="link" onClick={handleClick}>
+                        Checkout
+                    </button>
+                </form>
+            </main>
+        </div>
+    )
+}
